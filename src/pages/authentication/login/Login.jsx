8a23c4e3a0f9090d4cf021f783484f72b74c8bf0
Shuan Co <78131504+shuan-co@users.noreply.github.com>
2023-06-21 23:59:35 +0800

    login
    added

--- src/pages/authentication/login/Login.jsx
+++ src/pages/authentication/login/Login.jsx
@@ -1,12 +1,76 @@
 import "./login.css";
 
-import React from 'react';
-
+import React from "react";
 
 function Login() {
-    return (<>
-        <div><h1>Login Page</h1></div>
-    </>);
+  return (
+    <>
+      <div id="container-login">
+        <img
+          id="background-login"
+          src="./authentication/login/loginbg.png"
+          alt=""
+        />
+
+        <div id="container-auth-login">
+          <div id="container-auth-left-login">
+            <img
+              id="container-auth-leftbg-login"
+              className="floating"
+              src="./authentication/login/leftbg.png"
+              alt=""
+            />
+            <img
+              id="container-auth-leftbgshadow-login"
+              className="floating"
+              src="./authentication/login/leftbgshadow.png"
+              alt=""
+            />
+            <p id="container-auth-left-quote-login">
+              UNLEASH YOUR <br /> COSMIC CREATIVITY.
+            </p>
+          </div>
+          <div id="container-auth-right-login">
+            <div>
+              <p id="container-auth-right-header-login">Welcome Aboard!</p>
+              <p id="container-auth-right-lower-login">
+                What are your plans today, traveler?
+              </p>
+              <form id="container-auth-right-form-login" action="">
+                <label
+                  id="container-auth-right-label-login"
+                  htmlFor="container-auth-right-email-login"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name=""
+                  className="container-auth-right-input-login"
+                  id="container-auth-right-email-login"
+                  placeholder="sample@gmail.com"
+                />
+                <label
+                  id="container-auth-right-label-login"
+                  htmlFor="container-auth-right-email-login"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name=""
+                  placeholder="Minimum 8 characters"
+                  className="container-auth-right-input-login"
+                  id="container-auth-right-email-login"
+                />
+                <input id="container-auth-right-submit-login" type="submit" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
