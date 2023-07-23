009aeefec1064f8a77dd9cb9ec08c84a932ca173
Gregorio John Marc A <122092610+1Cealus@users.noreply.github.com>
2023-07-23 23:01:25 +0800

    Fixed duplicating glitch
    Removed setposts that put it on local storage

--- src/pages/homepage/components/Feed.jsx
+++ src/pages/homepage/components/Feed.jsx
@@ -84,7 +84,6 @@ function Feed({ posts: initialPosts = [], currentUser, deletePost }) {
         
           const docRef = await addDoc(collection(db, "posts"), newPost);
           console.log("Document written with ID: ", docRef.id);
-          setPosts(oldPosts => [...oldPosts, {...newPost, id: docRef.id}]);
         } catch (e) {
           console.error("Error adding document: ", e);
         }     
@@ -160,7 +159,6 @@ function Feed({ posts: initialPosts = [], currentUser, deletePost }) {
         
           const docRef = await addDoc(collection(db, "posts"), newPost);
           console.log("Document written with ID: ", docRef.id);
-          setPosts(oldPosts => [...oldPosts, {...newPost, id: docRef.id}]);
         } catch (e) {
           console.error("Error adding document: ", e);
         }
