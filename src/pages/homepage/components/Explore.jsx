eda784f90f75f3df145b6737289502c5685dcb1d
Gregorio John Marc A <122092610+1Cealus@users.noreply.github.com>
2023-07-23 16:52:33 +0800

    Updated explore jsx
    Updated explore jsx to finally work yay

--- src/pages/homepage/components/Explore.jsx
+++ src/pages/homepage/components/Explore.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
+import { collection, getDocs, query, limit } from 'firebase/firestore';
+import { db } from '../../../firebase/Firebase';
 
 function Explore() {
-  const users = [
-    { username: 'user1' },
-    { username: 'user2' },
-    { username: 'user3' }
-  ];
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usersCol = collection(db, 'users');
+      const q = query(usersCol, limit(5));
+      const querySnapshot = await getDocs(q);
+
+      setUsers(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <div style={{ width: "100%", marginTop: "5vh" }}>
-        <h2>Explore</h2>
-        {users.length > 0 ? (
-          users.map(user => (
-            <Card className="mb-3" key={user.username}>
-              <Card.Body>
-                <Card.Title>{user.username}</Card.Title>
-                <Card.Link href={`/profile/${user.username}`}>View Profile</Card.Link>
-              </Card.Body>
-            </Card>
-          ))
-        ) : (
-          <p>No users available.</p>
-        )}
+      <h2>Explore</h2>
+      {users.length > 0 ? (
+        users.map(user => (
+          <Card className="mb-3" key={user.id}>
+            <Card.Body>
+              <Card.Title>{user.firstname}</Card.Title> {/* Use firstname instead of username */}
+              <Card.Link href={`/profile/${user.id}`}>View Profile</Card.Link> {/* Use id instead of username */}
+            </Card.Body>
+          </Card>
+        ))
+      ) : (
+        <p>No users available.</p>
+      )}
     </div>
   );
 }
